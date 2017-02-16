7051412a10c79a6470757feda03378e22d43b44a
DESKTOP-N44B0D5\ulijah <uxc6625@g.rit.edu>
2017-02-16 15:20:00 -0500

    Downloaded Materials, 3 different platform scripts, Timer

---
+++ Assets/Scripts/Platform_Right.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Right : MonoBehaviour {
+
+    public float speed = 1;
+
+    private int direction = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            if (direction == 1)
+
+                direction = -1;
+
+
+
+            else
+                direction = 1;
+        }
+
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }
+
+
+
+    }
+
+
+}
