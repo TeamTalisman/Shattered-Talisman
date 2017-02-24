b8253730cf70aa8a77821dddb375943dcb545aab
Erick <ericksaurisyl@gmail.com>
2017-02-24 11:34:20 -0500

    Added GUI for showing time to continue game after falling

--- Assets/Scripts/Player/Timer.cs
+++ Assets/Scripts/Player/Timer.cs
@@ -17,6 +17,7 @@ public class Timer : MonoBehaviour {
   }
   void Start() {
     isTimerRunning = true;
+    waitTimer = WAIT_TIME;
   }
 
   // Update is called once per frame
@@ -24,7 +25,6 @@ public class Timer : MonoBehaviour {
 
     if (isTimerRunning) {
       playTimer -= Time.deltaTime;
-      
       // If player movement if locked
       if (cc.lockMovement) {
         // unlock it
@@ -34,16 +34,22 @@ public class Timer : MonoBehaviour {
           playTimer = 0;
           Invoke("KillPlayer", 1f);
       }
-    } else if (waitTimer >= WAIT_TIME ) {
+    } else if (waitTimer <= 0) {
       isTimerRunning = true;
-      waitTimer = 0f;
+      waitTimer = WAIT_TIME;
     } else {
-      waitTimer += Time.deltaTime;
+      waitTimer -= Time.deltaTime;
     }
   }
 
   private void OnGUI() {
+    float waitBoxX = (Screen.width / 2) - 100;
+    float waitBoxY = (Screen.height / 3) - 10;
     GUI.Box(new Rect(10,10,50,20),"" + playTimer.ToString("0"));
+
+    if (!isTimerRunning) {
+      GUI.Box(new Rect(waitBoxX, waitBoxY, 200, 20),"Time to continue: " + waitTimer.ToString("0"));
+    }
   }
 
   void KillPlayer() {
