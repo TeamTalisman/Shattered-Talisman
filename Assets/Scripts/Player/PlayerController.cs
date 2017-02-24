90b11024ee7852f91f3ba435d19d36eb2950a643
Erick <ericksaurisyl@gmail.com>
2017-02-24 00:29:17 -0500

    removed files

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public Checkpoint lastCheckpoint;
+
+	public int health = 3;
+
+	void Start() {
+		Init();
+	}
+	void Init() {
+		Spawn();
+	}
+
+	void Spawn() {
+		// If we don't have a checkpoint
+		if (lastCheckpoint == null) {
+			// Find the spawn point and get its Checkpoint script
+			lastCheckpoint = GameObject.FindGameObjectWithTag("SpawnPoint").
+												GetComponent<Checkpoint>();
+			// Collect the checkpoint
+			lastCheckpoint.Collect();
+		}
+
+		// Set the transform position of player to that of the checkpoint
+		SetPosition(lastCheckpoint.transform.position);
+	}
+
+	void SetPosition(Vector3 newPosition) {
+		transform.position = newPosition;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Void") {
+			CollideVoid();
+		} else if (other.tag == "CheckPoint") {
+			Checkpoint checkpoint = other.gameObject.GetComponent<Checkpoint>();
+			CollideCheckpoint(checkpoint);
+		} else if (other.tag == "EndPoint") {
+			Endpoint endpoint = other.gameObject.GetComponent<Endpoint>();
+			CollideEndPoint(endpoint);
+		}
+	}
+
+	void CollideVoid() {
+		Debug.Log("YOU FELL!");
+		health --;
+
+		if (health > 0) {
+			Spawn();
+		} else {
+			float fadeTime = gameObject.GetComponent<Fading>().BeginFade(1);
+			Invoke("GameOver", fadeTime);
+		}
+	}
+
+	void CollideCheckpoint(Checkpoint checkpoint) {
+		Debug.Log("YOU GOT A CHECKPOINT");
+		// Set out last checkpoint to checkpoint
+		lastCheckpoint = checkpoint;
+
+		// Collect checkpoint
+		lastCheckpoint.Collect();
+	}
+
+	void CollideEndPoint(Endpoint endpoint) {
+		Debug.Log("YOU FINISHED THE LEVEL!");
+		float fadeTime = gameObject.GetComponent<Fading>().BeginFade(1);
+		// Set out last checkpoint to checkpoint
+		// endpoint.Collect();
+		Invoke("EndLevel", fadeTime);
+	}
+
+	public void GameOver() {
+		SceneManager.LoadScene("GameOverScene");
+	}
+
+	public void EndLevel() {
+		SceneManager.LoadScene("EndScene");
+	}
+}
