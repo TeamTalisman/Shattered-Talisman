d01a47d768d41af98bbfd8c1cdb82f125036c623
DESKTOP-N44B0D5\ulijah <uxc6625@g.rit.edu>
2017-02-28 18:26:44 -0500

    Added Lava Environment & fixed shaking platform

---
+++ Assets/Scripts/Platform/Hold_Character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hold_Character : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider col)
+    {
+        col.transform.parent = gameObject.transform;
+
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        col.transform.parent = null;
+
+    }
+}
