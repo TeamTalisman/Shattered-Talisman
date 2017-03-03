4f6d2117830797255227daec9c445ce607815ced
Erick <ericksaurisyl@gmail.com>
2017-03-02 23:21:51 -0500

    Fixed issue with Timer. Changed Camera angle and distance,

--- Assets/Scripts/Player/Timer.cs
+++ Assets/Scripts/Player/Timer.cs
@@ -5,52 +5,55 @@ using Invector.CharacterController;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
-  public PlayerController player;
+  GameObject player;
+  public PlayerController playerController;
   protected vThirdPersonController cc; // access the ThirdPersonController component                
 
+  public Transform LoadingBar;
+  [SerializeField] private float currrentAmount;
+  [SerializeField] private float speed;
 
-
-    public Transform LoadingBar;
-    [SerializeField] private float currrentAmount;
-    [SerializeField] private float speed;
+  public float waitTimer = 0f;
+  float playTimer = 60.0f;
+  public float TIMER = 60.0f;
+  public bool isTimerRunning;
+  public static float WAIT_TIME = 2f;
 
   void Awake() {
-    cc = GetComponent<vThirdPersonController>();
+    player = GameObject.FindGameObjectWithTag("Player");
+    cc = player.GetComponent<vThirdPersonController>();
+    playerController = player.GetComponent<PlayerController>();
   }
-  void Start() {
   
+  void Start() {
+    isTimerRunning = true;
+    waitTimer = WAIT_TIME;
+    playTimer = TIMER;
   }
 
   // Update is called once per frame
   void Update () {
-
-        if (currrentAmount < 100 && currrentAmount >= 0)
-        {
-            currrentAmount += speed * Time.deltaTime;
-
-
-        }
-
-        else
-        {
-            Invoke("KillPlayer", 1f);
-        }
-        LoadingBar.GetComponent<Image>().fillAmount = currrentAmount / 100;
-
-
-
-
-
-
-
-
-   
+    if (isTimerRunning) {
+      playTimer -= Time.deltaTime;
+      LoadingBar.GetComponent<Image>().fillAmount = playTimer / TIMER;
+      // If player movement if locked
+      if (cc.lockMovement) {
+        // unlock it
+        cc.lockMovement = false;
+      }
+      if (playTimer <= 0) {
+          playTimer = 0;
+          Invoke("KillPlayer", 1f);
+      }
+    } else if (waitTimer <= 0) {
+      isTimerRunning = true;
+      waitTimer = WAIT_TIME;
+    } else {
+      waitTimer -= Time.deltaTime;
+    }
   }
 
-
-
-    private void KillPlayer()
-    {
-        player.GameOver();
-    }
+  private void KillPlayer() {
+    playerController.GameOver();
+  }
 }
